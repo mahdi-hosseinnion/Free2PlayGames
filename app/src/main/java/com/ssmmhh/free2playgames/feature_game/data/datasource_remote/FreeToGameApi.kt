1a84042b17c99939f12851e177c825f36ea2ffaa
mahdi <ssmmhh1382@gmail.com>
2022-02-12 18:23:28 +0330

    setup retrofit

---
+++ app/src/main/java/com/ssmmhh/free2playgames/feature_game/data/datasource_remote/FreeToGameApi.kt
@@ -0,0 +1,10 @@
+package com.ssmmhh.free2playgames.feature_game.data.datasource_remote
+
+import com.ssmmhh.free2playgames.feature_game.data.datasource_remote.dto.GameDto
+import retrofit2.http.GET
+
+interface FreeToGameApi {
+
+    @GET("/games")
+    suspend fun getGames(): List<GameDto>
+}
