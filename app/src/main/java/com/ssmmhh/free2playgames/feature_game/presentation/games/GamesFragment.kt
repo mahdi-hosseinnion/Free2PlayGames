a652a40bc8dc83a44636dc34339c8bd0b56ee0b5
mahdi <ssmmhh1382@gmail.com>
2022-02-20 21:34:03 +0330

    setup gameFragments basic functionality

--- app/src/main/java/com/ssmmhh/free2playgames/feature_game/presentation/games/GamesFragment.kt
+++ app/src/main/java/com/ssmmhh/free2playgames/feature_game/presentation/games/GamesFragment.kt
@@ -1,8 +1,93 @@
 package com.ssmmhh.free2playgames.feature_game.presentation.games
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.ssmmhh.free2playgames.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ssmmhh.free2playgames.databinding.FragmentGamesBinding
+import com.ssmmhh.free2playgames.feature_game.presentation.games.viewstate.GameListViewState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class GamesFragment : Fragment(R.layout.fragment_games) {}
+class GamesFragment
+    : Fragment() {
+
+    private val gamesRecyclerViewAdapter = GameListRecyclerViewAdapter()
+
+    private val viewModel by viewModels<GamesViewModel>()
+
+    private var _binding: FragmentGamesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGamesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+        subscribeCollectors()
+
+    }
+
+    private fun setupUI() {
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerGameList.apply {
+            layoutManager = LinearLayoutManager(this@GamesFragment.requireContext())
+            adapter = gamesRecyclerViewAdapter
+        }
+    }
+
+    private fun subscribeCollectors() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.gameListViewState.collect {
+                    handleRecyclerViewViewState(it)
+                }
+            }
+        }
+    }
+
+    private fun handleRecyclerViewViewState(vs: GameListViewState) {
+        if (vs.games.isNotEmpty()) {
+            binding.txtGameListError.visibility = View.GONE
+            binding.recyclerGameList.visibility = View.VISIBLE
+            gamesRecyclerViewAdapter.submitList(vs.games)
+        }
+        //handle loading
+        binding.prgGameList.visibility = if (vs.isLoading) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+        //handle error
+        if (!vs.errorMessage.isNullOrBlank()) {
+            binding.txtGameListError.visibility = View.VISIBLE
+            binding.recyclerGameList.visibility = View.GONE
+            binding.txtGameListError.text = vs.errorMessage
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/ssmmhh/free2playgames/feature_game/presentation/games/GamesViewModel.kt
+++ app/src/main/java/com/ssmmhh/free2playgames/feature_game/presentation/games/GamesViewModel.kt
@@ -6,18 +6,20 @@ import com.ssmmhh.free2playgames.feature_game.data.util.Result
 import com.ssmmhh.free2playgames.feature_game.domain.model.Game
 import com.ssmmhh.free2playgames.feature_game.domain.use_cases.GetGamesUseCase
 import com.ssmmhh.free2playgames.feature_game.presentation.games.viewstate.GameListViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class GamesViewModel
 @Inject
 constructor(
     private val getGamesUseCase: GetGamesUseCase
 ) : ViewModel() {
 
-    private val _gameListViewState = MutableStateFlow(GameListViewState())
+    private val _gameListViewState = MutableStateFlow(GameListViewState(isLoading = true))
     val gameListViewState: StateFlow<GameListViewState> = _gameListViewState
 
     init {
@@ -27,6 +29,9 @@ constructor(
     fun refreshGames() = getGames()
 
     private fun getGames() {
+        //set is loading to true
+        _gameListViewState.value = GameListViewState(isLoading = true)
+        //retrieve data from api
         viewModelScope.launch {
             val result = getGamesUseCase.invoke()
             _gameListViewState.value = when (result) {
